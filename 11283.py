843f4bba657139beab187d00c490740412395fcd
gon2gon2 <ark5354@gmail.com>
2021-12-27 17:50:02 +0900

    2021.12.27

---
+++ 11283.py
@@ -0,0 +1,16 @@
+# import sys
+# char = sys.stdin.readline().strip()
+# print(str(char.encode()).split('\\'))
+
+# 가
+# b'\xea\xb0\x80'
+# first = ["ㄱ", "ㄲ", "ㄴ", "ㄷ", "ㄸ", "ㄹ", "ㅁ", "ㅂ", "ㅃ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅉ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+# second = ["ㅏ", "ㅐ", "ㅑ", "ㅒ", "ㅓ", "ㅔ", "ㅕ", "ㅖ", "ㅗ", "ㅘ", "ㅙ", "ㅚ", "ㅛ", "ㅜ", "ㅝ", "ㅞ", "ㅟ", "ㅠ", "ㅡ", "ㅢ", "ㅣ"]
+# third = ["ㄱ", "ㄲ", "ㄳ", "ㄴ", "ㄵ", "ㄶ", "ㄷ", "ㄹ", "ㄺ", "ㄻ", "ㄼ", "ㄽ", "ㄾ", "ㄿ", "ㅀ", "ㅁ", "ㅂ", "ㅄ", "ㅅ", "ㅆ", "ㅇ", "ㅈ", "ㅊ", "ㅋ", "ㅌ", "ㅍ", "ㅎ"]
+# first = [str(i.encode()) for i in first]
+# second = [str(i.encode()) for i in second]
+# third = [str(i.encode()) for i in third]
+# print(first)
+
+# ord함수를 통해 아스키 코드로 변환해서 시작을 1로 맞춰주기만 하면 된다
+print(ord(input())-44031)
